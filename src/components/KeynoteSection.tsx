afb8131028f0f11d27f2aa55a537c2fc82c8db8f
srijithnair78 <srijithnair5182@gmail.com>
2026-09-11 01:47:52 +0400

    Update KeynoteSection.tsx

--- src/components/KeynoteSection.tsx
+++ src/components/KeynoteSection.tsx
@@ -27,11 +27,11 @@ export const KeynoteSection: React.FC = () => {
           className="relative order-1 lg:order-none"
         >
           <div className="absolute -inset-3 bg-gradient-to-br from-[#C99E5D]/20 to-transparent blur-2xl pointer-events-none" />
-          <div className="relative overflow-hidden border border-[#8C6D4F]/40">
+          <div className="relative overflow-hidden border border-[#8C6D4F]/40 bg-[#0C0A08]">
             <img
               src="/images/keynote.jpg"
               alt="Srijith Nair delivering a keynote"
-              className="w-full h-full object-cover aspect-[4/5]"
+              className="w-full h-full object-contain aspect-[3/2]"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-transparent to-transparent" />
           </div>
